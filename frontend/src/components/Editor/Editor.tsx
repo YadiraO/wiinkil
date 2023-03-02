de943c9ebd71259dd8dbbcb34fbad3cf2d1ec42e
JChicomeSan <57263663+ChicomeMX7@users.noreply.github.com>
2023-03-02 00:28:10 -0600

    ci: updated types and relations

--- frontend/src/components/Editor/Editor.tsx
+++ frontend/src/components/Editor/Editor.tsx
@@ -26,7 +26,7 @@ export const Editor: FC<EditorCustomProps> = ({
     const getDataInput = useCallback(() => {
         try {
             const newData = window.editor.getValue()
-            console.log(dat);
+            console.log(dat)
             setDat(newData)
         } catch {
             alert(
